b140e176569e366b1aded5ecf2e3d1f581e8ee36
utkarsh <98471140+ZION260122@users.noreply.github.com>
2024-07-07 16:49:59 +0530

    home page profile view without banner

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,50 @@
 import './home.scss'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <div className="home-container">
+        <div className="home-wrapper">
+          <div className="home">
+            <div className="home__leftsidebar">
+              <div className="home__leftsidebar__profile">
+                <div className="img">
+                  <img src="..\src\assets\profile.png" alt="" />
+                </div>
+                <div className="about">
+                   <h3>Micheal Jackson</h3>
+                   <h5>Dancer</h5>
+                   <p>3rd B.Tech Mechanical Engineering</p>
+                </div>
+                <div className="horizontalseparator"></div>
+                <div className="profilestatus">
+                    <div className="followers">
+                      <h3>Followers</h3>
+                      <p>1000</p>
+                      </div>
+                      <div className="verticalseparator"></div>
+                      <div className="following">
+                      <h3>Following</h3>
+                      <p>2000</p>
+                      </div>
+                </div>
+                <div className="horizontalseparator"></div>
+                <div className="profilelink">
+                  <a href="">Profile</a>
+                </div>
+              </div>
+            </div>
+            <div className="home__middlesidebar">
+              <div className="home__middlesidebar__scrollbar">
+                 status
+              </div>
+            </div>
+            <div className="home__rightsidebar"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
