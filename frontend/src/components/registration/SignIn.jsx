b5b28daf6998542a8be6407633b6a204e9e809d6
utkarsh <98471140+ZION260122@users.noreply.github.com>
2024-07-14 23:26:37 +0530

    Sign In and Sign UP updation with chakraUI

--- frontend/src/components/registration/SignIn.jsx
+++ frontend/src/components/registration/SignIn.jsx
@@ -1,38 +1,67 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Image,
+  Input,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import "./registration.scss";
 
-const SignIn = () => {
+const SignUp = () => {
+  const formWidth = useBreakpointValue({ base: "90%", md: "35rem" });
+  const imageDisplay = useBreakpointValue({ base: "none", md: "block" });
+  const flexDirection = useBreakpointValue({ base: "column", md: "row" });
+
   return (
-    <div>
-      <div className="registration-container">
-        <div className="registration-form__wrapper">
-          <h2>Sign In</h2>
-          <form className="registration-form">
-            <div className="registration-form__group">
-            <label>Email</label>
-              <input
-                type="email"
-                placeholder="Enter your college Email"
-                name="email"
-                required
-              />
-            </div>
-            <div className="registration-form__group">
-            <label>password</label>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                name="password"
-                required
-              />
-            </div>
-            <div className="registration-form__group">
-              <button className="registration-button">Sign In</button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
+    <>
+      <Flex justifyContent="center">
+        <Flex mt={20} flexDirection={flexDirection}>
+          <Flex
+            h="25rem"
+            justifyContent="center"
+            display={imageDisplay}
+          >
+            <Box w="40rem">
+              <Image height="25rem" src="..\src\assets\profile.png" />
+            </Box>
+          </Flex>
+          <Flex
+            gap={5}
+            h="25rem"
+            w={formWidth}
+            bg="gray.600"
+            alignItems="center"
+            direction="column"
+            p={5}
+          >
+            <Text mt={5} mb={3} fontSize="20px" textAlign="center">
+              Build Your Professional Life in Society
+            </Text>
+          
+            <Flex w="100%">
+              <Flex marginX={2} flex="1">
+                <Input type="email" placeholder="College Email ID" />
+              </Flex>
+            </Flex>
+            <Flex w="100%">
+              <Flex marginX={2} flex="1">
+                <Input type="password" placeholder="Password" />
+              </Flex>
+            </Flex>
+            <Button>Create Account</Button>
+            <Flex gap={2} fontSize="13px">
+              <Text>Already have an account?</Text>
+              <Text cursor="pointer" color="blue.500">Sign In</Text>
+            </Flex>
+            <Divider />
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
   );
 };
 
-export default SignIn;
+export default SignUp;
